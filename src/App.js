62c299774497756963b41dc301ebb5bd792ea2ea
Thomas Aggerholm <thomasaggerholm@hotmail.com>
2019-11-30 17:51:34 +0100

    App.js commited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Route } from 'react-router-dom';
+
+import Home from './pages/home';
+import About from './pages/about';
+
+const routes = [
+    { path: "/", name: "Home", Component: Home },
+    { path: "/about", name: "About", Component: About }
+  ];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {routes.map(({path, Component})=> {
+            <Route key={path} exact path={path}>
+                <div className="page">
+                    <Component />
+                </div>
+            </Route>
+        })}
     </div>
   );
 }
